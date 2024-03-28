5dd3867b5e247c660dad553a13d1ec260b924fe7
a <ronbajrami2005@gmail.com>
2024-03-28 18:13:17 -0400

    k

--- src/globals.ts
+++ src/globals.ts
@@ -6,7 +6,7 @@
 export const MY_BU_ID = "";
 export const BASE_API_URL = "https://spark-se-assessment-api.azurewebsites.net/api";
 // You can get this from Gradescope aka x-functions-key
-export const TOKEN = "";
+export const TOKEN = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ==";
 // This is a helper function to generate the headers with the x-functions-key attached
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
