28a2c0ff0ce1c2d7451acb1958705ebc008e8e97
Christopher R <39552369+TheRealchr1s@users.noreply.github.com>
2024-03-28 18:15:46 -0400

    dd

--- src/globals.ts
+++ src/globals.ts
@@ -10,7 +10,6 @@ export const TOKEN = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ==";
 // This is a helper function to generate the headers with the x-functions-key attached
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
-  // You will need to add another header here
-  // If you do not, the API will reject your request (:
+  headers.append("x-functions-key", TOKEN);
   return headers;
 };
